3f6516207b286c07c6c5cf8fd76f4e7c3716efbe
Daniel Norman <daniel@danielnorman.net>
2021-11-30 22:08:12 -0800

    optimize day1

--- src/challenge/day1p1.rs
+++ src/challenge/day1p1.rs
@@ -1,22 +1,18 @@
 pub fn run() {
     let (input, mut _aoc) = super::get(1);
+    let lines: Vec<u32> = input.lines().map(|l| l.parse().unwrap()).collect();
 
-    let mut increase_count = 0u32;
-    let mut last_depth = None;
-    for line in input.lines() {
-        let depth: u32 = line.parse().unwrap();
-
-        match last_depth {
-            None => {}
-            Some(l) => {
-                if depth > l {
-                    increase_count += 1;
+    let increase_count =
+        lines.windows(2).fold(
+            0u32,
+            |ac, window| {
+                if window[1] > window[0] {
+                    ac + 1
+                } else {
+                    ac
                 }
-            }
-        }
-        last_depth = Some(depth)
-    }
-
+            },
+        );
     println!("increase_count: {}", increase_count);
     // _aoc.submit(&increase_count.to_string()).unwrap();
 }

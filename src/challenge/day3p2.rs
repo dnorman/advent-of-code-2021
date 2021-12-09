ec7e8b308301c0a4ed272a95ca3ac6487890a280
Daniel Norman <daniel@danielnorman.net>
2021-12-08 18:56:42 -0800

    solve day3p2

--- src/challenge/day3p2.rs
+++ src/challenge/day3p2.rs
@@ -1,27 +1,26 @@
+const WIDTH: u8 = 12;
 pub fn run() {
     let (input, mut _aoc) = super::get(3);
 
-    let input = "00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010"
-        .to_string();
+    //     let input = "00100
+    // 11110
+    // 10110
+    // 10111
+    // 10101
+    // 01111
+    // 00111
+    // 11100
+    // 10000
+    // 11001
+    // 00010
+    // 01010"
+    //         .to_string();
 
-    let mut bit_counts = [0u32; 12];
     let lines: Vec<usize> = input
         .lines()
         .map(|line| usize::from_str_radix(line, 2).unwrap())
         .collect();
 
-    let (avg_bits_up, avg_bits_down) = avg_bits(&lines);
     // The bit criteria depends on which type of rating value you want to find:
 
     // To find oxygen generator rating, determine the most common value (0 or 1) in the current bit position,
@@ -29,21 +28,28 @@ pub fn run() {
     // a 1 in the position being considered.
 
     let mut oxygen_generator_rating = 0usize;
-    let mut bit_criteria = 0usize;
-    'og_scan: for offset in 0..12 {
-        // Add one bit at a time to the bit_criteria
-        let avg_bit_is_one = query_bit(avg_bits_up, offset);
-        bit_criteria |= (avg_bit_is_one as usize) << offset;
+    {
+        let mut og_lines = lines.clone();
+        'og_scan: for bit in (0..WIDTH).rev() {
+            // Add one bit at a time to the bit_criteria
 
-        match match_line(&lines, bit_criteria, offset) {
-            Match::One(line) => {
-                oxygen_generator_rating = line;
+            let (avg_bits_up, _) = avg_bits(&og_lines);
+            let match_bit = query_bit(avg_bits_up, bit);
+            println!("OG scan {}: {:b},{:?}", bit, avg_bits_up, match_bit);
+
+            og_lines.retain(|line| match_bit == query_bit(*line, bit));
+
+            for line in og_lines.iter() {
+                println!("\t{:05b} ", line);
+            }
+            if og_lines.len() == 1 {
+                oxygen_generator_rating = og_lines[0];
                 break 'og_scan;
             }
-            Match::Many => {} // keep going
-            Match::None => panic!("no oxygen generator rating found"),
         }
     }
+
+    println!("oxygen_generator_rating: {}", oxygen_generator_rating);
 
     // To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit position,
     // and keep only numbers with that bit in that position. If 0 and 1 are equally common, keep values with
@@ -54,23 +60,29 @@ pub fn run() {
     // Otherwise, repeat the process, considering the next bit to the right.
 
     let mut co2_scrubber_rating = 0usize;
-    let mut bit_criteria = 0usize;
-    'co_scan: for offset in 0..12 {
-        // Add one bit at a time to the bit_criteria
-        let avg_bit_is_one = query_bit(avg_bits_down, offset);
-        bit_criteria |= (avg_bit_is_one as usize) << offset;
+    {
+        let mut co2_lines = lines.clone();
+        'co2_scan: for bit in (0..WIDTH).rev() {
+            // Add one bit at a time to the bit_criteria
 
-        match match_line(&lines, bit_criteria, offset) {
-            Match::One(line) => {
-                co2_scrubber_rating = line;
-                break 'co_scan;
+            let (avg_bits_up, _) = avg_bits(&co2_lines);
+            let match_bit = !query_bit(avg_bits_up, bit);
+            println!("CO2 scan {}: {:b},{:?}", bit, avg_bits_up, match_bit);
+
+            co2_lines.retain(|line| match_bit == query_bit(*line, bit));
+
+            for line in co2_lines.iter() {
+                println!("\t{:05b} ", line);
             }
-            Match::Many => {} // keep going
-            Match::None => panic!("no co2 scrubber rating found"),
+            if co2_lines.len() == 1 {
+                co2_scrubber_rating = co2_lines[0];
+                break 'co2_scan;
+            }
         }
     }
 
-    println!("oxygen_generator_rating: {}", oxygen_generator_rating);
+    println!("co2_scrubber_rating: {}", co2_scrubber_rating);
+
     let life_support_rating = oxygen_generator_rating * co2_scrubber_rating;
 
     assert_eq!(life_support_rating, 1);
@@ -78,32 +90,13 @@ pub fn run() {
 }
 
 fn query_bit(num: usize, bit: u8) -> bool {
-    (num & 1 << bit) != 0
+    ((num >> bit) & 1) != 0
 }
 
 enum Match {
     One(usize),
     Many,
     None,
-}
-fn match_line(lines: &[usize], mask: usize, extent: u8) -> Match {
-    let mut last_matched_line = None;
-    'line: for line in lines {
-        for offset in 0..=extent {
-            if query_bit(mask, offset) != query_bit(*line, offset) {
-                continue 'line;
-            }
-        }
-        if last_matched_line.is_some() {
-            return Match::Many;
-        }
-        last_matched_line = Some(*line)
-    }
-
-    match last_matched_line {
-        Some(line) => Match::One(line),
-        None => Match::None,
-    }
 }
 
 // Is binary averaging like, a remotely normal thing to do?
@@ -116,7 +109,7 @@ fn avg_bits(lines: &[usize]) -> (usize, usize) {
     let mut bit_counts = [0usize; 12];
     for line in lines {
         for i in 0..12 {
-            let one = (line & 1 << i) != 0;
+            let one = (line >> i & 1) != 0;
             bit_counts[i] += one as usize; // cast boolean to 0 or 1
         }
     }
@@ -128,10 +121,10 @@ fn avg_bits(lines: &[usize]) -> (usize, usize) {
         let zeros = total_lines - ones;
 
         if *ones > zeros {
-            avg_bits_round_up |= 0b1 << i;
-            avg_bits_round_down |= 0b1 << i;
+            avg_bits_round_up |= (0b1 << i);
+            avg_bits_round_down |= (0b1 << i);
         } else if *ones == zeros {
-            avg_bits_round_up |= 0b1 << i;
+            avg_bits_round_up |= (0b1 << i);
         }
         // (else) round down is the default
     }

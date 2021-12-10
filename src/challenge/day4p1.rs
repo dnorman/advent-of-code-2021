eb46527424ed4ffbbcdb9968b20478a354c9926a
Daniel Norman <daniel@danielnorman.net>
2021-12-10 01:07:52 -0800

    complete day4p1

--- src/challenge/day4p1.rs
+++ src/challenge/day4p1.rs
@@ -1,28 +1,71 @@
+use std::convert::TryInto;
+
+use itertools::Itertools;
+
 pub fn run() {
     let (input, mut _aoc) = super::get(4);
-    let input = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+    // let input = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
-    22 13 17 11  0
-     8  2 23  4 24
-    21  9 14 16  7
-     6 10  3 18  5
-     1 12 20 15 19
-    
-     3 15  0  2 22
-     9 18 13 17  5
-    19  8  7 25 23
-    20 11 10 24  4
-    14 21 16 12  6
-    
-    14 21 17 24  4
-    10 16 15  9 19
-    18  8 23 26 20
-    22 11 13  6  5
-     2  0 12  3  7";
+    // 22 13 17 11  0
+    //  8  2 23  4 24
+    // 21  9 14 16  7
+    //  6 10  3 18  5
+    //  1 12 20 15 19
 
-    for line in input.lines() {
-        println!("{}", line)
-    }
+    //  3 15  0  2 22
+    //  9 18 13 17  5
+    // 19  8  7 25 23
+    // 20 11 10 24  4
+    // 14 21 16 12  6
+
+    // 14 21 17 24  4
+    // 10 16 15  9 19
+    // 18  8 23 26 20
+    // 22 11 13  6  5
+    //  2  0 12  3  7";
+
+    let mut lines = input.lines().into_iter();
+
+    let draws: Vec<u8> = lines
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+    // println!("{:?}", draws);
+
+    let mut boards: Vec<_> = lines
+        .chunks(6)
+        .into_iter()
+        .map(|chunk| {
+            let squares: Vec<_> = chunk
+                .flat_map(|line| {
+                    line.split(" ").filter(|v| v.len() > 0).map(|v| Square {
+                        number: v.parse().unwrap(),
+                        marked: false,
+                    })
+                })
+                .collect();
+            Board::new(squares.try_into().unwrap())
+        })
+        .collect();
+
+    let winner = draws.iter().find_map(|num| {
+        println!("DRAW: {}", num);
+        for board in boards.iter_mut() {
+            board.mark_number(*num);
+            if board.is_winning() {
+                let score = board.score() * *num as usize;
+                println!("WIN: {}", score);
+                println!("{}", board.pretty());
+
+                return Some(score);
+            }
+        }
+        None
+    });
+
+    assert_eq!(winner, Some(33348))
 }
 
 #[derive(Clone, Debug)]
@@ -30,6 +73,8 @@ struct Square {
     number: u8,
     marked: bool,
 }
+
+#[derive(Clone, Debug)]
 struct Board {
     squares: [Square; 25],
 }
@@ -42,6 +87,13 @@ impl Board {
         let squares = squares.map(|s| s.into());
         Board { squares }
     }
+    fn mark_number(&mut self, number: u8) {
+        for square in &mut self.squares {
+            if square.number == number {
+                square.marked = true;
+            }
+        }
+    }
     fn is_winning(&self) -> bool {
         // rows
         self.squares[..]
@@ -51,9 +103,30 @@ impl Board {
         // columns
         || (0..5).any(|col| (0..5).all(|row| self.squares[(5 * row) + col].marked))
 
-        // diagonals
-        || [0, 6, 12, 18, 24].iter().all(|i| self.squares[*i].marked)
-        || [4, 8, 12, 16, 20].iter().all(|i| self.squares[*i].marked)
+        // // diagonals
+        // || [0, 6, 12, 18, 24].iter().all(|i| self.squares[*i].marked)
+        // || [4, 8, 12, 16, 20].iter().all(|i| self.squares[*i].marked)
+    }
+    fn score(&self) -> usize {
+        self.squares
+            .iter()
+            .fold(0, |a, s| if !s.marked { a + s.number as usize } else { a })
+    }
+    fn pretty(&self) -> String {
+        let mut out = String::new();
+        for chunk in self.squares[..].chunks(5) {
+            for square in chunk.iter() {
+                out.push_str(&square.number.to_string());
+
+                if square.marked {
+                    out.push_str("X\t");
+                } else {
+                    out.push_str("\t");
+                }
+            }
+            out.push('\n');
+        }
+        out
     }
 }
 
